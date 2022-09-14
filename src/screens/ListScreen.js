cd80c99e9659a3dc5df96d865125259997bea67f
Deepak Prasad <prasad_951945@student.nitw.ac.in>
2022-09-14 23:49:56 +0530

    initial working

---
+++ src/screens/ListScreen.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const ListScreen = () => {
+    return <Text>List Sceen</Text>
+}
+
+const style = StyleSheet.create({})
+
+export default ListScreen
